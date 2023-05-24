3c220fa2c1aa197d512268556208ca9c2e5463ad
danoon3 <danoone58@gmail.com>
2023-05-24 14:34:43 +0300

    changed SearchEngine and Main

--- src/main/java/BooleanSearchEngine.java
+++ src/main/java/BooleanSearchEngine.java
@@ -1,18 +1,49 @@
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.canvas.parser.PdfTextExtractor;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class BooleanSearchEngine implements SearchEngine {
-    //???
+    protected final Map<String, List<PageEntry>> data = new HashMap<>();
 
     public BooleanSearchEngine(File pdfsDir) throws IOException {
-        // прочтите тут все pdf и сохраните нужные данные,
-        // тк во время поиска сервер не должен уже читать файлы
+        List<PageEntry> pageEntries;
+        for (File file : pdfsDir.listFiles()) {
+            if (file.getName().contains(".pdf")) {
+                try (var doc = new PdfDocument(new PdfReader(file))) {
+                    for (int i = 1; i < doc.getNumberOfPages(); i++) {
+                        String text = PdfTextExtractor.getTextFromPage(doc.getPage(i));
+                        String[] words = text.split("\\P{IsAlphabetic}+");
+                        Set<String> setWords = Arrays.stream(words)
+                                .map(String::toLowerCase)
+                                .collect(Collectors.toSet());
+                        for (String word : setWords) {
+                            int count = Collections.frequency(Arrays.stream(words).collect(Collectors.toList()), word);
+                            PageEntry pageEntry = new PageEntry(file.getName(), i, count);
+                            if (!data.containsKey(word)) {
+                                pageEntries = new ArrayList<>();
+                                pageEntries.add(pageEntry);
+                                data.put(word, pageEntries);
+                            } else {
+                                data.get(word).add(pageEntry);
+                            }
+                            data.get(word).sort(Collections.reverseOrder());
+                        }
+                    }
+                } catch (IOException e) {
+                    System.out.println("Не могу прочитать документ");
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     @Override
     public List<PageEntry> search(String word) {
-        // тут реализуйте поиск по слову
-        return Collections.emptyList();
+        return data.get(word.toLowerCase());
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,34 @@
-import java.io.File;
-import java.util.Arrays;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Main {
+    protected static final int PORT = 8989;
+
     public static void main(String[] args) throws Exception {
         BooleanSearchEngine engine = new BooleanSearchEngine(new File("pdfs"));
-        System.out.println(engine.search("бизнес"));
-
-        // здесь создайте сервер, который отвечал бы на нужные запросы
-        // слушать он должен порт 8989
-        // отвечать на запросы /{word} -> возвращённое значение метода search(word) в JSON-формате
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+            while (true) {
+                try (Socket socket = serverSocket.accept();
+                     PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                     BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+                    String words = in.readLine();
+                    GsonBuilder builder = new GsonBuilder();
+                    Gson gson = builder.create();
+                    var pageEntry = engine.search(words);
+                    if (pageEntry == null) {
+                        out.println("Ничего не удалось найти");
+                    } else {
+                        out.println(gson.toJson(pageEntry));
+                    }
+                }
+            }
+        } catch (IOException e) {
+            System.out.println("Сервер не запускается");
+            e.printStackTrace();
+        }
     }
 }
